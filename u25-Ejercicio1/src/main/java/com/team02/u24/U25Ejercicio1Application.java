ccadf78e35efbfe8dc6207ca89f612bd9ea9d6ab
Jose Antonio González Alcántara <jagascripts@gmail.com>
2022-05-16 09:37:10 +0200

    Creado el proyecto en eclipse Spring tools

---
+++ u25-Ejercicio1/src/main/java/com/team02/u24/U25Ejercicio1Application.java
@@ -0,0 +1,13 @@
+package com.team02.u24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U25Ejercicio1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U25Ejercicio1Application.class, args);
+	}
+
+}
---
+++ u25-Ejercicio1/src/test/java/com/team02/u24/U25Ejercicio1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team02.u24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U25Ejercicio1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
