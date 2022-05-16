7162ee0feb129a59226073d966ce14080f66dbb4
diablillo99lp <joan.hurtado.garcia@gmail.com>
2022-05-16 10:33:03 +0200

    DAO

---
+++ u25-Ejercicio1/src/main/java/com/team02/u25/ejercicio1/dao/IFabricanteDAO.java
@@ -0,0 +1,9 @@
+package com.team02.u25.ejercicio1.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.team02.u25.ejercicio1.dto.Fabricante;
+
+public interface IFabricanteDAO extends JpaRepository<Fabricante, Long>{
+
+}
